b44d70b68f0d6ab88547b6c909f153baa86c6940
pedro <pierre.veysseyre@epitech.eu>
2021-03-06 05:33:01 +0100

    update .cmake

--- Tools/Tools.cmake
+++ Tools/Tools.cmake
@@ -5,7 +5,7 @@ get_filename_component(ToolsDir ${CMAKE_CURRENT_LIST_FILE} PATH)
 # Enable tools individually
 option(TOOL_INTERPRETER "Build tests" OFF)
 
-include(${ToolsDir}/PartitionDecoder/PartitionDecoder.cmake)
+# include(${ToolsDir}/PartitionDecoder/PartitionDecoder.cmake)
 
 if(TOOL_INTERPRETER)
     include(${ToolsDir}/Interpreter/Interpreter.cmake)
