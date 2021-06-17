29b999fb6d72ce6bedd4280a580347ecfda6e235
Pierre Veysseyre <pierre.veysseyre@epitech.eu>
2021-06-17 21:50:01 +0200

    Update tests

--- Tests/tests_Reformater.cpp
+++ Tests/tests_Reformater.cpp
@@ -18,42 +18,81 @@ using F64 = double;
 
 TEST(Reformater, FromFloatingBounds)
 {
-    F32 in { -1.0f };
+    F32 f32in { -1.0f };
     F64 f64out { 3.0 };
     U8 u8out { 3 };
     S8 s8out { 3 };
 
-    Reformater::Reformat<F32, F64>(&in, &f64out, 1u);
+    Reformater::Reformat<F32, F64>(&f32in, &f64out, 1u);
     ASSERT_EQ(f64out, -1.0);
-    Reformater::Reformat<F32, U8>(&in, &u8out, 1u);
+    Reformater::Reformat<F32, U8>(&f32in, &u8out, 1u);
     ASSERT_EQ(u8out, 0);
-    Reformater::Reformat<F32, S8>(&in, &s8out, 1u);
+    Reformater::Reformat<F32, S8>(&f32in, &s8out, 1u);
     ASSERT_EQ(s8out, -127);
 
 
-    in = { 1.0f };
+    f32in = { 1.0f };
     f64out = { 3.0 };
     u8out = { 3 };
     s8out = { 3 };
 
-    Reformater::Reformat<F32, F64>(&in, &f64out, 1u);
+    Reformater::Reformat<F32, F64>(&f32in, &f64out, 1u);
     ASSERT_EQ(f64out, 1.0);
-    Reformater::Reformat<F32, U8>(&in, &u8out, 1u);
+    Reformater::Reformat<F32, U8>(&f32in, &u8out, 1u);
     ASSERT_EQ(u8out, 255);
-    Reformater::Reformat<F32, S8>(&in, &s8out, 1u);
+    Reformater::Reformat<F32, S8>(&f32in, &s8out, 1u);
     ASSERT_EQ(s8out, 127);
 
 
-    in = { 0.0f };
+    f32in = { 0.0f };
     f64out = { 3.0 };
     u8out = { 3 };
     s8out = { 3 };
 
-    Reformater::Reformat<F32, F64>(&in, &f64out, 1u);
+    Reformater::Reformat<F32, F64>(&f32in, &f64out, 1u);
     ASSERT_EQ(f64out, 0.0);
-    Reformater::Reformat<F32, U8>(&in, &u8out, 1u);
+    Reformater::Reformat<F32, U8>(&f32in, &u8out, 1u);
     ASSERT_EQ(u8out, 127);
-    Reformater::Reformat<F32, S8>(&in, &s8out, 1u);
+    Reformater::Reformat<F32, S8>(&f32in, &s8out, 1u);
     ASSERT_EQ(s8out, 0);
 
+
+
+    F64 f64in { -1.0f };
+    F32 f32out { 3.0f };
+    u8out = { 3 };
+    s8out = { 3 };
+
+    Reformater::Reformat<F64, F32>(&f64in, &f32out, 1u);
+    ASSERT_EQ(f32out, -1.0f);
+    Reformater::Reformat<F64, U8>(&f64in, &u8out, 1u);
+    ASSERT_EQ(u8out, 0);
+    Reformater::Reformat<F64, S8>(&f64in, &s8out, 1u);
+    ASSERT_EQ(s8out, -127);
+
+
+    f64in = { 1.0 };
+    f32out = { 3.0f };
+    u8out = { 3 };
+    s8out = { 3 };
+
+    Reformater::Reformat<F64, F32>(&f64in, &f32out, 1u);
+    ASSERT_EQ(f32out, 1.0f);
+    Reformater::Reformat<F64, U8>(&f64in, &u8out, 1u);
+    ASSERT_EQ(u8out, 255);
+    Reformater::Reformat<F64, S8>(&f64in, &s8out, 1u);
+    ASSERT_EQ(s8out, 127);
+
+
+    f64in = { 0.0 };
+    f32out = { 3.0f };
+    u8out = { 3 };
+    s8out = { 3 };
+
+    Reformater::Reformat<F64, F32>(&f64in, &f32out, 1u);
+    ASSERT_EQ(f32out, 0.0f);
+    Reformater::Reformat<F64, U8>(&f64in, &u8out, 1u);
+    ASSERT_EQ(u8out, 127);
+    Reformater::Reformat<F64, S8>(&f64in, &s8out, 1u);
+    ASSERT_EQ(s8out, 0);
 }
